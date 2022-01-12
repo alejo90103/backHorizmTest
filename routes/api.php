e96b41d6d67015a1060b93e7ee7da3ad866f5af0
Alejandro Herrera <alejo901003@hotmail.com>
2022-01-13 00:17:01 +0100

    ratingAllPosts function

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/posts', [TestController::class, 'allPosts']);
+Route::get('/users', [TestController::class, 'allUsers']);
+Route::get('/rating/{word}', [TestController::class, 'ratingPost']);
+Route::get('/rating', [TestController::class, 'ratingAllPosts']);
